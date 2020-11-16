57353bb84d2fa458a7b9e3a7ee0ab1987d10df41
SravaniTeeparthi29 <57581683+SravaniTeeparthi29@users.noreply.github.com>
2020-11-16 16:40:52 -0700

    Create run_generate_tfrecord.sh

---
+++ HandDetection in Videos(Custom Dataset) using Single Shot Detector(TensorFlow)/generate_tfrecord/run_generate_tfrecord.sh
@@ -0,0 +1,2 @@
+# Training
+python generate_tfrecord.py --csv /home/sravani/Softwares/tensorflow-handdetection/csv_files/trn.csv --label hand --output /home/sravani/Softwares/tensorflow-handdetection/handtracking/training_aolme_new/train.record --images /home/sravani/Softwares/tensorflow-handdetection/aolme_frames
